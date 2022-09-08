8565fecc8fdb574d0354c3ea95b4393dfde8c148
ida <idax7478@stud.kea.dk>
2022-09-08 13:39:10 +0200

    ny

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Parafont: "Nunito Sans', sans-serif",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
