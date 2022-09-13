cb334e6fd1b31716072ad52514c03e96d7b7a1ad
ida <idax7478@stud.kea.dk>
2022-09-13 11:21:02 +0200

    ny

--- src/main.js
+++ src/main.js
@@ -13,23 +13,21 @@ inView(".fotogalleryinview", ({ target }) => {
 });
 
 /*----Sektion 5-----*/
-inView( ".h3inview" , () =>{
-  animate( ".h3inview", { y: [-30, 0 ] }, { duration: 1} )
-})
+inView(".h3inview", () => {
+  animate(".h3inview", { y: [-30, 0] }, { duration: 1 });
+});
 
 const items = document.querySelectorAll("#horisontalliste li");
 scroll(
-   animate("#horisontalliste", {
-     transform: ["none", `translateX(-${items.length - 1}00vw)`]
-   }),
-   { target: document.querySelector("#horisontalscrollsection") }
- ); 
+  animate("#horisontalliste", {
+    transform: ["none", `translateX(-${items.length - 1}00vw)`],
+  }),
+  { target: document.querySelector("#horisontalscrollsection") }
+);
 /*------sektion 5 slut------*/
-document.querySelectorAll(".parallaxcontainer").forEach(( parallaxcontainer ) => {
+document.querySelectorAll(".parallaxcontainer").forEach((parallaxcontainer) => {
   const elementderskalparallaxes = parallaxcontainer.querySelector("p");
   scroll(animate(elementderskalparallaxes, { y: [100, 1000] }), {
-    target: elementderskalparallaxes
+    target: elementderskalparallaxes,
   });
 });
-
-
